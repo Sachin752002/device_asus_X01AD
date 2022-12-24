3b45530bf007d0d07e7dc073ca8d84f2ce10ae10
Sachin752002 <103373100+Sachin752002@users.noreply.github.com>
2022-12-24 08:44:25 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X01AD.mk
+    $(LOCAL_DIR)/awaken_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_X01AD-user \
-    evolution_X01AD-userdebug \
-    evolution_X01AD-eng
+    awaken_X01AD-user \
+    awaken_X01AD-userdebug \
+    awaken_X01AD-eng
